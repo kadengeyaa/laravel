6cf4fa99d31cbbd666802790554141be8ddb3ba1
Kadenge Yaa <kadengeyaa@gmail.com>
2021-02-12 14:28:17 +0300

    Registration completed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -29,6 +30,8 @@ class RegisterController extends Controller
             'email'    => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        Auth::attempt($request->only('email', 'password'));
 
         return redirect()->route('dashboard');
     }
