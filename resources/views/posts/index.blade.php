c7b99b579839f415b607ce8bce9faab08fcad1dd
Kadenge Yaa <kadengeyaa@gmail.com>
2021-02-12 20:27:26 +0300

    Lists of posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,9 +18,20 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium">Post</button>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium mb-6">Post</button>
                 </div>
             </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                        <p class="mb-2">{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
