04120f673240002309365d837365c79d0924e9db
Kadenge Yaa <kadengeyaa@gmail.com>
2021-02-12 21:27:44 +0300

    Liking completed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,14 +30,17 @@
                         <p class="mb-2">{{ $post->body }}</p>
 
                         <div class="flex items-center">
-                            <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                            <form action="" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Unlike</button>
-                            </form>
+                            @if ($post->likedBy(auth()->user()))
+                                <form action="" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form> 
+                            @endif
 
                             <span>{{ $post->likes->count() }} {{ Str::plural("like", $post->likes->count()) }}</span>
                         </div>
